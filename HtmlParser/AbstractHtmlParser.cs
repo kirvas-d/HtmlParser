5fbd9a13eab01690d441f24ec451fcd06af96bc0
Денис <@DESKTOP-EKTHKC1>
2021-04-25 17:53:24 +0300

    Добавьте файлы проекта.

---
+++ HtmlParser/AbstractHtmlParser.cs
@@ -0,0 +1,36 @@
+﻿using HtmlParser.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser
+{
+    public class AbstractHtmlParser<TEntity>
+    {
+        private IHtmlScraperService htmlScraperService;
+        private IHtmlParserService<TEntity> htmlParserService;
+
+        public AbstractHtmlParser(IHtmlParserService<TEntity> htmlParserService, IHtmlScraperService htmlScraperService) 
+        {
+            this.htmlParserService = htmlParserService;
+            if (this.htmlParserService == null) 
+            {
+                throw new Exception("HtmlParserService равен null");
+            }
+
+            this.htmlScraperService = htmlScraperService;
+            if (this.htmlScraperService == null)
+            {
+                throw new Exception("HtmlParserService равен null");
+            }
+        }
+
+        public IEnumerable<TEntity> GetEntity() 
+        {
+            throw new Exception();
+        }
+
+    }
+}
